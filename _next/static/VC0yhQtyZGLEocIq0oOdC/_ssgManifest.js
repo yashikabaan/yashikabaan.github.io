43bacba672cd68abe325f35eddcf7a42ad542fd8
yashikabaan <yashikabaan@users.noreply.github.com>
2026-09-18 00:21:55 +0000

    deploy: yashikabaan/portfolio@35e5f0bdfedccb2ac7aa66a07cc8c31336f78081

---
+++ _next/static/VC0yhQtyZGLEocIq0oOdC/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fportfolio\u002F[id]","\u002Fprojects\u002F[ID]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/16396lxukfcvv.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,55277,function(o){o.n(o.i(83764))}]);
